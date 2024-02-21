daab7a07ca3d6e84ef9186396bd67e047b38a31e
Anderson Souza <anderson.nunes.souza@hotmail.com>
2024-02-21 14:48:01 -0300

    - bitcoin5years - realizada chamada para 4 dias e os 4 parâmetros retornados salvos no Mongodb

--- coinMarketCall.py
+++ coinMarketCall.py
@@ -1,16 +1,32 @@
 import requests
 from datetime import date
+import json
+from pymongo import MongoClient
+
 
 def bitcoin5years():
-    request = 'https://rest.coinapi.io/v1/ohlcv/BITSTAMP_SPOT_BTC_USD/history?apikey=DC035A41-F1B1-4EF9-AD10-0349474586C3&period_id=1DAY&time_start=2021-01-01T00:00:00&limit=360'
+    print('bitcoin5years')
+    request = 'https://rest.coinapi.io/v1/ohlcv/BITSTAMP_SPOT_BTC_USD/history?apikey=DC035A41-F1B1-4EF9-AD10-0349474586C3&period_id=1DAY&time_start=2021-01-01T00:00:00&limit=4'
     r = requests.get(request)
-    print("teste")
-    print(r.status_code)
-    return 
+    if r.status_code == 200:
+        data_dict = json.loads(r.text)
+        # Conectando-se ao MongoDB
+        client = MongoClient('localhost', 27017)
+        db = client['coinmarketcap']
+        collection = db['bitcoin_data']
+        # Inserindo o dicionário na coleção do MongoDB
+        collection.insert_many(data_dict)
+        return True
+    else:
+        print('Erro ao fazer a solicitação HTTP:', r.status_code)
+        return False
+    
 
 def bitcoinLast360days():
-    startday = date.today(-1)
-    requests = '"https://rest.coinapi.io/v1/ohlcv/BITSTAMP_SPOT_BTC_USD/history?apikey=DC035A41-F1B1-4EF9-AD10-0349474586C3&period_id=1DAY&time_start=' + startday +'&time_end={endday}'
+    print("bitcoinLast360days")
+    # startDay = date.today(-1)
+    # endDay = date.month()
+    # requests = '"https://rest.coinapi.io/v1/ohlcv/BITSTAMP_SPOTs_BTC_USD/history?apikey=DC035A41-F1B1-4EF9-AD10-0349474586C3&period_id=1DAY&time_start=' + startDay +'&time_end={endday}'
     return
 
 bitcoin5years()
