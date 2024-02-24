2ddfb1f453f6e2c0ff6fff221b44af191baae61c
Anderson Souza <anderson.nunes.souza@hotmail.com>
2024-02-24 00:09:34 -0300

    - bitcoin5years() e bitcoinLast360days()
    - Configurado datetime para utilização nos parâmetros "start_time" e "end_time" do endpoint da API
    
    - As linhas a seguir reportam alteração para salvar a informação no BD, separarei os dois dados em 2 collections diferentes no Mongodb para evitar sobrescrita de dados.
        - alterado o valor da variável collection para "collection = db['bitcoin_data_last_5_years']" para a função "bitcoin_data_last_5_years()"
        - alterado o valor da variável collection para "collection = db['bitcoin_data_last_360_days'] para a função "bitcoin_data_last_360_days()"

--- coinMarketCall.py
+++ coinMarketCall.py
@@ -1,33 +1,60 @@
 import requests
-from datetime import date
+import datetime
 import json
 from pymongo import MongoClient
 
-
 def bitcoin5years():
-    print('bitcoin5years')
-    request = 'https://rest.coinapi.io/v1/ohlcv/BITSTAMP_SPOT_BTC_USD/history?apikey=DC035A41-F1B1-4EF9-AD10-0349474586C3&period_id=1DAY&time_start=2021-01-01T00:00:00&limit=4'
+    data_atual = datetime.datetime.now()
+    # Subtraia 5 anos da data atual
+    data_menos_5_anos = data_atual - datetime.timedelta(days=5*365)
+    # Formate a data no formato ISO 8601
+    data_formatada = data_menos_5_anos.replace(microsecond=0).isoformat()
+    # Refatorar data atual para formato ISO 8601
+    data_atual = datetime.datetime.now().replace(microsecond=0).isoformat()
+    request = f"https://rest.coinapi.io/v1/ohlcv/BITSTAMP_SPOT_BTC_USD/history?apikey=DC035A41-F1B1-4EF9-AD10-0349474586C3&period_id=1MTH&time_start={data_formatada}&limit=360&time_end={data_atual}"
     r = requests.get(request)
+    #Validar se a requisição retorna 200
     if r.status_code == 200:
         data_dict = json.loads(r.text)
         # Conectando-se ao MongoDB
         client = MongoClient('localhost', 27017)
         db = client['coinmarketcap']
-        collection = db['bitcoin_data']
+        collection = db['bitcoin_data_last_5_years']
         # Inserindo o dicionário na coleção do MongoDB
         collection.insert_many(data_dict)
+        print("Informações adicionadas ao Collection bitcoin_data_last_5_years! - Verifique o seu MongoDB")
         return True
     else:
         print('Erro ao fazer a solicitação HTTP:', r.status_code)
-        return False
+    return 
     
 
 def bitcoinLast360days():
     print("bitcoinLast360days")
-    # startDay = date.today(-1)
-    # endDay = date.month()
-    # requests = '"https://rest.coinapi.io/v1/ohlcv/BITSTAMP_SPOTs_BTC_USD/history?apikey=DC035A41-F1B1-4EF9-AD10-0349474586C3&period_id=1DAY&time_start=' + startDay +'&time_end={endday}'
-    return
+    data_atual = datetime.datetime.now()
+    # Subtraia 1 ano da data atual
+    data_menos_1_ano = data_atual - datetime.timedelta(days=360)
+    # Formate a data no formato ISO 8601
+    data_formatada = data_menos_1_ano.replace(microsecond=0).isoformat()
+    # Refatorar data atual para formato ISO 8601
+    data_atual = datetime.datetime.now().replace(microsecond=0).isoformat()
+    request = f'https://rest.coinapi.io/v1/ohlcv/BITSTAMP_SPOT_BTC_USD/history?apikey=DC035A41-F1B1-4EF9-AD10-0349474586C3&period_id=1DAY&time_start={data_formatada}&limit=360&time_end={data_atual}'
+    print(request)
+    r = requests.get(request)
+    #Validar se a requisição retorna 200
+    if r.status_code == 200:
+        data_dict = json.loads(r.text)
+        # Conectando-se ao MongoDB
+        client = MongoClient('localhost', 27017)
+        db = client['coinmarketcap']
+        collection = db['bitcoin_data_last_360_days']
+        # Inserindo o dicionário na coleção do MongoDB
+        collection.insert_many(data_dict)
+        print("Informações adicionadas ao Collection bitcoin_data_last_360_days! - Verifique o seu MongoDB")
+        return True
+    else:
+        print('Erro ao fazer a solicitação HTTP:', r.status_code)
+    return 
 
 bitcoin5years()
 
